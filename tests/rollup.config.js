268f64bb22b07c484b7f6fa263041dfb3c325d29
Markus Felten <Markus.Felten@gmx.de>
2017-10-22 13:31:52 +0200

    chore: use built/*-test.js for the generated test cases

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,19 +1,11 @@
-import babel from 'rollup-plugin-babel';
-
 export default {
   input: 'tests/run-test.js',
   external: ['ava'],
 
-  plugins: [
-    babel({
-      babelrc: false,
-      presets: ['stage-3'],
-      exclude: 'node_modules/**'
-    })
-  ],
+  plugins: [],
 
   output: {
-    file: 'build/test-bundle.js',
+    file: 'build/run-test.js',
     format: 'cjs',
     sourcemap: true
   }
