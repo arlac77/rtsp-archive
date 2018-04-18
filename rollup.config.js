5ff2977a4dc5bbb7404b05c555d7b35755b5a47a
Markus Felten <Markus.Felten@gmx.de>
2018-04-18 20:33:36 +0200

    chore: use rollup-plugin-executable

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import executable from 'rollup-plugin-executable';
 import pkg from './package.json';
 
 export default {
@@ -9,7 +10,7 @@ export default {
     format: 'cjs',
     banner: '#!/usr/bin/env node'
   },
-  plugins: [nodeResolve(), commonjs(), json()],
+  plugins: [nodeResolve(), commonjs(), json(), executable()],
   external: ['fs', 'util', 'child_process', 'path', 'config-expander'],
   input: pkg.module
 };
