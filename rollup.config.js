18fe72202da6166b9dff0492e4ecdb9ab6b6caba
Markus Felten <Markus.Felten@gmx.de>
2018-08-06 01:16:58 +0200

    chore(package): add rollup-plugin-cleanup@^3.0.0 chore(package): semantic-release@^15.9.5 chore(package): travis-deploy-once@^5.0.2 chore(rollup): import cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import cleanup from 'rollup-plugin-cleanup';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
