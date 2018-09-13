43c818efc09e151e9b7f373c821fdd33eef38c4a
Markus Felten <Markus.Felten@gmx.de>
2018-09-13 16:18:43 +0200

    fix(rollup): generate node call with --experimental-modules --experimental-worker

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import cleanup from 'rollup-plugin-cleanup';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import executable from 'rollup-plugin-executable';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import executable from "rollup-plugin-executable";
+import pkg from "./package.json";
 
 export default {
   output: {
-    file: pkg.bin['rtsp-archive'],
-    format: 'cjs',
-    banner: '#!/usr/bin/env node',
+    file: pkg.bin["rtsp-archive"],
+    format: "cjs",
+    banner: "#!/usr/bin/env node --experimental-modules --experimental-worker",
     interop: false
   },
   plugins: [resolve(), commonjs(), json(), cleanup(), executable()],
-  external: ['fs', 'util', 'child_process', 'path', 'config-expander'],
+  external: ["fs", "util", "child_process", "path", "config-expander"],
   input: pkg.module
 };
