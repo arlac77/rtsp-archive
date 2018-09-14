6d38312063b25a39401639e77682da168886864a
Markus Felten <Markus.Felten@gmx.de>
2018-09-14 02:13:11 +0200

    fix(rollup): env -S

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default {
   output: {
     file: pkg.bin["rtsp-archive"],
     format: "cjs",
-    banner: "#!/usr/bin/env node --experimental-modules --experimental-worker",
+    banner: "#!/usr/bin/env -S node --experimental-modules --experimental-worker",
     interop: false
   },
   plugins: [resolve(), commonjs(), json(), cleanup(), executable()],
