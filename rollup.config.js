601b9632c0293402f7baadfcd64a913d5a11ee86
Markus Felten <Markus.Felten@gmx.de>
2018-10-02 15:04:20 +0200

    fix(cli): make env working on linux too

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default {
     file: pkg.bin["rtsp-archive"],
     format: "cjs",
     banner:
-      "#!/usr/bin/env -S node --experimental-modules --experimental-worker",
+      '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-worker "$0" "$@"',
     interop: false
   },
   plugins: [
