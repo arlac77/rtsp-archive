0ccf514f2329edb78075fa7f5235a938deffa6cb
Markus Felten <Markus.Felten@gmx.de>
2019-05-12 20:07:09 +0200

    chore(rollup): make use of builtins

--- rollup.config.js
+++ rollup.config.js
@@ -6,48 +6,8 @@ import executable from "rollup-plugin-executable";
 import json from "rollup-plugin-json";
 import pkg from "./package.json";
 
-// require('module').builtinModules
-const external = [
-  "assert",
-  "async_hooks",
-  "buffer",
-  "child_process",
-  "cluster",
-  "console",
-  "constants",
-  "crypto",
-  "dgram",
-  "dns",
-  "domain",
-  "events",
-  "fs",
-  "http",
-  "http2",
-  "https",
-  "inspector",
-  "module",
-  "net",
-  "os",
-  "path",
-  "perf_hooks",
-  "process",
-  "punycode",
-  "querystring",
-  "readline",
-  "repl",
-  "stream",
-  "string_decoder",
-  "sys",
-  "timers",
-  "tls",
-  "trace_events",
-  "tty",
-  "url",
-  "util",
-  "v8",
-  "vm",
-  "zlib"
-];
+
+const external = [...builtins];
 
 export default {
   output: {
