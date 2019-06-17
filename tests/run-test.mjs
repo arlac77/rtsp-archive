8c6721fe7c9a9dcdf0fc1cc7f8288ee2ec857164
Markus Felten <Markus.Felten@gmx.de>
2019-06-17 16:06:53 +0200

    test: use *.mjs

---
+++ tests/run-test.mjs
@@ -0,0 +1,24 @@
+import test from "ava";
+import { join, dirname } from "path";
+import execa from "execa";
+import { fileURLToPath } from "url";
+
+const here = dirname(fileURLToPath(import.meta.url));
+
+test("rtsp-archive", async t => {
+  const result = await execa(join(here, "..", "bin", "rtsp-archive"), [
+    "-h"
+  ]);
+  t.regex(result.stdout, /--config <dir>/);
+});
+
+/*
+test('rtsp-archive with config', async t => {
+  return execa(path.join(here, '..', 'bin', 'rtsp-archive'), [
+    `--configx=${path.join(here,'..','config','config.json')}`, '-h'
+  ]).then(result => {
+    console.log(result.stdout);
+    //=> 'unicorns'
+  });
+});
+*/
