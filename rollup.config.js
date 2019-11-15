21c6d87f001c2a4e030985c7438ece7a8ce13919
Markus Felten <Markus.Felten@gmx.de>
2019-11-15 02:58:50 +0100

    chore(rollup): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,53 @@
-import builtins from "builtin-modules";
-import cleanup from "rollup-plugin-cleanup";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import executable from "rollup-plugin-executable";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
+import cleanup from "rollup-plugin-cleanup";
+import builtins from "builtin-modules";
 import pkg from "./package.json";
 
-
 const external = [...builtins];
+const extensions = ["js", "mjs", "jsx", "tag"];
+const plugins = [
+  commonjs(),
+  resolve(),
+  json({
+  //  include: "package.json",
+    preferConst: true,
+    compact: true
+  }),
+  cleanup({
+    extensions
+  })
+];
 
-export default {
-  output: {
-    file: pkg.bin["rtsp-archive"],
-    format: "cjs",
-    banner:
-      '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"',
+const config = Object.keys(pkg.bin || {}).map(name => {
+  return {
+    input: `src/${name}-cli.mjs`,
+    output: {
+      plugins: [executable()],
+      banner:
+        '#!/bin/sh\n":" //# comment; exec /usr/bin/env node --experimental-modules --experimental-wasm-modules "$0" "$@"',
+      file: pkg.bin[name]
+    }
+  };
+});
+
+if (pkg.module !== undefined && pkg.main !== undefined) {
+  config.push({
+    input: pkg.module,
+    output: {
+      file: pkg.main
+    }
+  });
+}
+
+export default config.map(c => {
+  c.output = {
     interop: false,
-    externalLiveBindings: false
-  },
-  plugins: [
-    resolve({ preferBuiltins: true }),
-    commonjs(),
-    json({
-      include: "package.json",
-      preferConst: true,
-      compact: true
-    }),
-    cleanup({
-      extensions: ['js','mjs','jsx','tag']
-    }),
-    executable()
-  ],
-  external,
-  input: pkg.module
-};
+    externalLiveBindings: false,
+    format: "cjs",
+    ...c.output
+  };
+  return { plugins, external, ...c };
+});
