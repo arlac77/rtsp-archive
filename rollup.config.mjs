07c60b935d6dc010071b02233ab36b1f4ee3dc8b
Markus Felten <Markus.Felten@db.com>
2020-04-11 12:21:38 +0200

    fix(rollup): sync with template

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,7 +8,7 @@ import consts from "rollup-plugin-consts";
 
 import builtins from "builtin-modules";
 
-const { name, version, description, main, module, bin } = JSON.parse(
+const { name, version, description, bin } = JSON.parse(
   readFileSync("./package.json", { encoding: "utf8" })
 );
 
@@ -24,35 +24,21 @@ const plugins = [
   resolve(),
   native(),
   cleanup({
-    extensions: ["js", "mjs"],
-  }),
+    extensions: ["js", "mjs"]
+  })
 ];
 
-const config = Object.keys(bin).map((name) => {
+export default Object.keys(bin).map(name => {
   return {
     input: `src/${name}-cli.mjs`,
     output: {
       plugins: [executable()],
       file: bin[name],
+      interop: false,
+      externalLiveBindings: false,
+      format: "cjs"
     },
+    plugins,
+    external
   };
-});
-
-if (module !== undefined && main !== undefined) {
-  config.push({
-    input: module,
-    output: {
-      file: main,
-    },
-  });
-}
-
-export default config.map((c) => {
-  c.output = {
-    interop: false,
-    externalLiveBindings: false,
-    format: "cjs",
-    ...c.output,
-  };
-  return { plugins, external, ...c };
 });
