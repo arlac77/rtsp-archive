aed0260658dd82e195d64c4e111e1b72b8ab3de2
Markus Felten <Markus.Felten@gmx.de>
2025-12-12 21:30:55 +0000

    fix: sync entry point

--- src/rtsp-archive-cli.mjs
+++ src/rtsp-archive-cli.mjs
@@ -8,16 +8,15 @@ const args = process.argv.slice(2);
 
 switch (args[0]) {
   case "--version":
-      console.log(pkg.version);
-      process.exit(0);
-
+    console.log(pkg.version);
+    process.exit(0);
   case "--help":
   case "-h":
-      console.log(`${pkg.description} (${pkg.version})
+    console.log(`${pkg.description} (${pkg.version})
 usage:
  -h --help this help screen
  -c --config <directory> set config directory`);
-      process.exit(0);
+    process.exit(0);
 
   case "--config":
   case "-c":
@@ -31,14 +30,17 @@ async function initializeServiceProvider() {
   try {
     let serviceProvider;
     try {
-      const m = await import("@kronos-integration/service-systemd");
-      serviceProvider = new m.default();
+      serviceProvider = new (
+        await import("@kronos-integration/service-systemd")
+      ).default();
     } catch (e) {
-      serviceProvider = new StandaloneServiceProvider(
-        JSON.parse(
-          readFileSync(join(args[1], "config.json"), "utf8")
-        )
-      );
+      if (e.error === "ERR_MODULE_NOT_FOUND") {
+        serviceProvider = new StandaloneServiceProvider(
+          JSON.parse(readFileSync(join(args[1], "config.json"), "utf8"))
+        );
+      } else {
+        throw e;
+      }
     }
 
     await initialize(serviceProvider);
